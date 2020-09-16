c630d2305cc78b16b09f7bae12a68a8111da0bf3
Bruno Felipe <bruno.felipe7170@gmail.com>
2020-09-16 13:10:11 -0300

    Mobile completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,48 @@
 const openMenu = document.querySelector('.menu__mobile-circle');
 const closeMenu = document.querySelector('.menu__mobile-close');
 const menuMobile = document.querySelector('.menu__mobile-content');
+const menuMobileItems = document.querySelectorAll('.menu__mobile-item');
+let activeMenuMobile = document.querySelector('.menu__mobile-active');
+const arrowsMobile = document.querySelectorAll('.icon-arrow');
 
 openMenu.addEventListener('click', function() {
-    menuMobile.style.left = '0%';
+    menuMobile.style.left = '0%';   
     closeMenu.style.zIndex = 1001;
 })
 
 closeMenu.addEventListener('click', function() {
-    menuMobile.style.left = '150%';
-    closeMenu.style.zIndex = 5;
+    closeMenuMobile()
 })
 
 
 var mySwiper = new Swiper('.swiper-container', {
-    speed: 1500,
+    speed: 250,
     spaceBetween: 100,
     autoHeight: true,
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      },
+    effect: 'slide',
+    loop: 'true'
 });
+
+
+menuMobileItems.forEach(function(menuMobileItem) {
+    menuMobileItem.addEventListener('click', function(e) {
+        e.preventDefault();
+        activeMenuMobile.classList.remove('menu__mobile-active');
+        menuMobileItem.classList.add('menu__mobile-active');
+        console.log(menuMobileItem.dataset.id)
+        mySwiper.slideTo(menuMobileItem.dataset.id)
+        activeMenuMobile = menuMobileItem;
+        closeMenuMobile();
+    })
+})
+
+arrowsMobile.forEach(function(arrows) {
+    arrows.addEventListener('click', function() {
+        mySwiper.slideTo(arrows.dataset.id)
+    })
+})
+
+function closeMenuMobile() {
+    menuMobile.style.left = '150%';
+    closeMenu.style.zIndex = 5;
+}
